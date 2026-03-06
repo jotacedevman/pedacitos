4a2e325876e9e5b378ba097c1af2285e18b5bd45
jotacedevman <jotacedevman@gmail.com>
2026-03-06 04:11:21 -0300

    [update][add] Added license head to python/hello_world.py. (#3)
    -- Added CC0 1.0 Universal head to python/hello_world.py. (#3)

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# This work is marked CC0 1.0 Universal.
+# To view a copy of this mark,
+# visit https://creativecommons.org/publicdomain/zero/1.0/
+
 ''' Estrutura padrão de execução de script.
     Função principal. Útil quando trabalhando com,
     aplicativos mais complexos ou com classes em POO. '''
