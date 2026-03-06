7fe8aed706cd19c4cd1a13404a8b6b666349f545
jotacedevman <jotacedevman@gmail.com>
2026-03-06 03:59:23 -0300

    [new][add] Created Python and ShellScript folder. (#2)
    -- Created Python and ShellScript folder
      -- Added hello_world.py on python/ .

---
+++ python/hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+''' Estrutura padrão de execução de script.
+    Função principal. Útil quando trabalhando com,
+    aplicativos mais complexos ou com classes em POO. '''
+
+def main():
+    print('Hello World!')
+
+if __name__ == "__main__":
+    main()
