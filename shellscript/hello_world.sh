7e9728880556633a8e57e35ee5bfeff3e0a906dd
jotacedevman <jotacedevman@gmail.com>
2026-03-10 06:43:32 -0300

    [update][add] Added hello_world.sh and renamed mountiso.sh on shellscript scripts. (#5)
    -- Added hello_world.sh and renamed mountiso.sh to mount_iso.sh on shellscript folder.

---
+++ shellscript/hello_world.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# This work is marked CC0 1.0 Universal.
+# To view a copy of this mark,
+# visit https://creativecommons.org/publicdomain/zero/1.0/
+
+# Estrutura padrão de execução de script.
+# Função principal. Útil quando trabalhando com,
+# aplicativos mais complexos. '''
+
+
+# Safe mode
+set -euo pipefail
+
+main () {
+  echo "Hello world!"
+}
+
+main
